d9edf2c8e9a015499b450aeeefbc22bb6d981185
Thomas Miller <thomas.miller@cheddar.com>
2019-11-18 09:02:51 -0500

    Possible solution to render from server

--- src/Components/Track.tsx
+++ src/Components/Track.tsx
@@ -1,19 +1,29 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { Context, ContextType } from '../Form'
 import { Box, Button } from 'grommet'
 import { Close } from 'grommet-icons'
+import parse from 'html-react-parser'
 
 type Props = {
   key: number
   item: any
 }
 
-//<iframe src="https://open.spotify.com/embed/track/6K8zRwaiHUv93In5P0TPwT" width="300" height="80" frameBorder="0" allowTransparency={true} allow="encrypted-media"/>
-
+      //<iframe
+        //src={`https://open.spotify.com/embed/track/${item.url}`}
+        //width="300"
+        //height="80"
+        //frameBorder="0"
+        //allow="encrypted-media"/>
 const Track: React.FC<Props> = (props) => {
+  const [track, setTrack] = useState('')
   const context = (useContext(Context) as ContextType)
   const { onClick } = context
   const { item } = props
+
+  useEffect(() => {
+    setTrack(JSON.parse(item.url))
+  }, [track])
 
   return (
     <Box direction='row' align='center'>
@@ -22,7 +32,7 @@ const Track: React.FC<Props> = (props) => {
         icon={<Close/>}
         onClick={() => onClick(item.url)}
       />
-        { item.url }
+        { parse(track) }
     </Box>
   )
 }
